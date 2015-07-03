f42b1037b80f5b5219a9ab57e1d61b27131fa55d
Carlos Andres Oquendo <caoquendo@outlook.com>
2015-07-02 22:26:12 -0500

    Added some types to the type library.

---
+++ App.TypeLib/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App.TypeLib
+{
+    public class Person
+    {
+        public String Name { get; set; }
+        public String LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
